d5af4cd906f4fad8339c05ce9d3ce77c20984271
SweepyBoop <78008331+SweepyBoop@users.noreply.github.com>
2024-03-16 14:18:41 -0700

    Update SortedArrayModified.h

--- Sorting/SortedArrayModified.h
+++ Sorting/SortedArrayModified.h
@@ -30,3 +30,43 @@ vector<int> sortedSquares(vector<int>& nums) {
     }
     return ans;
 }
+
+// https://leetcode.com/problems/sort-transformed-array/description/
+vector<int> sortTransformedArray(vector<int>& nums, int a, int b, int c) {
+    int n = nums.size();
+    for (int i = 0; i < n; ++i) {
+        int num = nums[i];
+        nums[i] = a * num * num + b * num + c;
+    }
+
+    // if a == 0, then we have a straight line
+    // if a < 0, we have asc + desc array; if a > 0, we have desc + asc array
+    // use 2 pointers to iterate elements
+    vector<int> ans(n, 0);
+    int left = 0;
+    int right = n - 1;
+    if (a < 0) {
+        // we are going from smallest element
+        // since we need exactly n iterations, we can use that to determine iteration end instead of left meeting right
+        for (int i = 0; i < n; ++i) {
+            if (nums[left] < nums[right]) {
+                ans[i] = nums[left++];
+            }
+            else {
+                ans[i] = nums[right--];
+            }
+        }
+    }
+    else {
+        // we are going from largest element, therefore need to reverse at the end
+        // this includes the case where a == 0, regardless of b value, we're going from largest value
+        for (int i = n - 1; i >= 0; --i) {
+            if (nums[left] < nums[right]) {
+                ans[i] = nums[right--];
+            }
+            else {
+                ans[i] = nums[left++];
+            }
+        }
+    }
+}
