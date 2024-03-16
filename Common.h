a495db1225aa9d066aae0d037d4e810c8d421a33
SweepyBoop <78008331+SweepyBoop@users.noreply.github.com>
2024-03-16 16:14:04 -0700

    Update

--- Common.h
+++ Common.h
@@ -1,4 +1,5 @@
 #pragma once
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
 using namespace std;
--- Hash Table/DestroySequentialTargets.h
+++ Hash Table/DestroySequentialTargets.h
@@ -2,5 +2,26 @@
 #include "..\Common.h"
 
 int destroyTargets(vector<int>& nums, int space) {
+    // keep track of nums[i] % space
+    // elements that can be destroyed share the same remainder
+    // we want to find which remainder has the most elements, then find out the smallest element that belongs to a group with mostElements
+    unordered_map<int, int> remainderMap;
+    int mostElements = 0; // most elements held in a remainder group, 2 groups could have the same elements
+    for (int num : nums) {
+        int remainder = num % space;
+        ++remainderMap[remainder];
+        if (remainderMap[remainder] > mostElements) {
+            mostElements = remainderMap[remainder];
+        }
+    }
 
+    // now iterate through nums again, if a num belongs to a remainder group with the most elements, it is potentially an answer
+    // we still need to find the smallest value
+    int ans = INT_MAX;
+    for (int num : nums) {
+        if (remainderMap[num % space] == mostElements) {
+            ans = min(ans, num);
+        }
+    }
+    return ans;
 }
