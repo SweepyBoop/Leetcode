4c8604a273581703fe93cda00eab641259c3bc93
SweepyBoop <78008331+SweepyBoop@users.noreply.github.com>
2024-03-16 15:22:21 -0700

    Create MinimumTimeToRepairCars.h

---
+++ Binary Search/MinimumTimeToRepairCars.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <vector>
+using namespace std;
+
+// https://leetcode.com/problems/minimum-time-to-repair-cars/
+// Given x minutes, we can check how many cars can be fixed in total and whether it >= cars
+// Do a binary search, but what initial range do we define?
+// max rank = 100, max cars = 10^6, so max x = 100 * pow(10^6, 2) = 10^14
+long long repairCars(vector<int>& ranks, int cars) {
+    int n = ranks.size();
+    long long left = 0;
+    long long right = 1e14;
+    while (left < right) {
+        int mid = (left + right) / 2;
+        // given mid minutes, how many cars can we repair
+        long long repair = 0;
+        for (int rank : ranks) {
+            repair += sqrt(mid / rank);
+        }
+        
+        if (repair < cars) {
+            left = mid + 1;
+        }
+        else {
+            right = mid;
+        }
+    }
+
+    return left;
+}
