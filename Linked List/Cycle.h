9c426cfa1073b5a68be7a55163ae92a3efea4953
SweepyBoop <78008331+SweepyBoop@users.noreply.github.com>
2024-03-18 21:57:28 -0700

    Create Cycle.h

---
+++ Linked List/Cycle.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "..\Common.h"
+
+// https://leetcode.com/problems/linked-list-cycle/description/?envType=study-plan-v2&envId=top-interview-150
+bool hasCycle(ListNode* head) {
+    // fast & slow runner, if the list has a cycle, they will meet at some point
+    if (!head || !head->next || !head->next->next) {
+        return false;
+    }
+
+    ListNode* slow = head;
+    ListNode* fast = head;
+    while (fast->next && fast->next->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast) {
+            return true;
+        }
+    }
+
+    return false;
+}
