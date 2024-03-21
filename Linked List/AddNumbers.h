571bb9542baad251f4b140444eb55f3b72b970ba
SweepyBoop <78008331+SweepyBoop@users.noreply.github.com>
2024-03-20 17:02:29 -0700

    Update AddNumbers.h

--- Linked List/AddNumbers.h
+++ Linked List/AddNumbers.h
@@ -23,3 +23,44 @@ ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
 
     return sentinel->next;
 }
+
+ListNode* reverseList(ListNode* head) {
+    if (!head || !head->next) {
+        return head;
+    }
+
+    ListNode* prev = nullptr;
+    while (head) {
+        ListNode* next = head->next;
+        head->next = prev;
+        prev = head;
+        head = next;
+    }
+    return prev;
+}
+
+// https://leetcode.com/problems/add-two-numbers-ii/
+ListNode* addTwoNumbersII(ListNode* l1, ListNode* l2) {
+    ListNode* r1 = reverseList(l1);
+    ListNode* r2 = reverseList(l2);
+
+    // note the direction we're constructing the result list, higher digit->next = prev digit
+    ListNode* prevDigit = nullptr;
+    int carryover = 0;
+    while (r1 || r2 || carryover) {
+        int tmp = (r1 ? r1->val : 0) + (r2 ? r2->val : 0) + carryover;
+        int digit = tmp % 10;
+        carryover = tmp / 10;
+        ListNode* currentDigit = new ListNode(digit, prevDigit);
+        prevDigit = currentDigit;
+
+        if (r1) {
+            r1 = r1->next;
+        }
+
+        if (r2) {
+            r2 = r2->next;
+        }
+    }
+    return prevDigit;
+}
