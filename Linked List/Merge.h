79b1ffdcafd7c38a6c8e81522238fad71c06e135
SweepyBoop <78008331+SweepyBoop@users.noreply.github.com>
2024-03-20 20:39:54 -0700

    Update Merge.h

--- Linked List/Merge.h
+++ Linked List/Merge.h
@@ -20,3 +20,32 @@ ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
 
     return sentinel->next;
 }
+
+// https://leetcode.com/problems/merge-k-sorted-lists/
+ListNode* mergeKLists(vector<ListNode*>& lists) {
+    // iterate through all lists and find the one with smallest element
+    // terminate when all lists are empty
+    // keep track of the index of the list that has the smallest value in each iter
+    int n = lists.size();
+    ListNode* sentinel = new ListNode();
+    ListNode* prev = sentinel;
+    while (true) {
+        int minIndex = -1; // if no element left, this will be -1 and terminate the loop
+        for (int i = 0; i < n; ++i)
+        {
+            if (minIndex == -1 || (lists[i] && lists[i]->val < lists[minIndex]->val)) {
+                minIndex = i;
+            }
+        }
+
+        if (minIndex == -1) {
+            break;
+        }
+
+        prev->next = lists[minIndex];
+        lists[minIndex] = lists[minIndex]->next;
+        prev = prev->next;
+    }
+
+    return sentinel->next;
+}
