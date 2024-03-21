d0ddf76685cee38de70f74d5e34f61916b7d88be
SweepyBoop <78008331+SweepyBoop@users.noreply.github.com>
2024-03-20 22:00:14 -0700

    Update Clone.h

--- Linked List/Clone.h
+++ Linked List/Clone.h
@@ -32,3 +32,54 @@ Node* copyRandomList(Node* head) {
     }
     return mapping[head];
 }
+
+Node* copyRandomListImproved(Node* head) {
+    // A->B->C->D
+    // interwined with original nodes
+    // A->A'->B->B'->C->C'->D->D'
+    Node* iter = head;
+    while (iter) {
+        Node* next = iter->next;
+        Node* clone = new Node(iter->val);
+        iter->next = clone;
+        clone->next = next;
+
+        iter = next;
+    }
+
+    // set the random pointers, now each original's next is always the cloned node
+    iter = head;
+    while (iter) {
+        Node* clone = iter->next;
+        Node* next = clone->next;
+
+        if (iter->random) {
+            clone->random = iter->random->next;
+        }
+
+        iter = next;
+    }
+
+    // now we no longer need original->next to locate the cloned nodes
+    // set next pointers for the cloned list
+    // and restore the next pointers for the original list
+    Node* sentinel = new Node(0);
+    Node* prev = sentinel;
+    while (head) {
+        Node* clone = head->next;
+        Node* next = clone->next;
+
+        if (next) {
+            clone->next = next->next;
+        }
+
+        head->next = next;
+        head = next;
+
+        // construct result list
+        prev->next = clone;
+        prev = prev->next;
+    }
+
+    return sentinel->next;
+}
