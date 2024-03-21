1e57f4ca76aa4d173daa756959b1a37678f198bb
SweepyBoop <78008331+SweepyBoop@users.noreply.github.com>
2024-03-20 23:01:41 -0700

    Update Reverse.h

--- Linked List/Reverse.h
+++ Linked List/Reverse.h
@@ -64,3 +64,45 @@ ListNode* reverseBetween(ListNode* head, int left, int right) {
 
     return sentinel->next;
 }
+
+// https://leetcode.com/problems/reverse-nodes-in-k-group/description/?envType=study-plan-v2&envId=top-interview-150
+ListNode* reverseKGroup(ListNode* head, int k) {
+    // we need to do n / k operations, e.g., n = 5, k = 2, then 2 reverse of size 2
+    int n = 0;
+    ListNode* iter = head;
+    while (iter) {
+        ++n;
+        iter = iter->next;
+    }
+
+    // each iteration: lead -> start..end -> tail (start..end) has k nodes
+    //               =>lead -> end..start -> tail
+    ListNode* sentinel = new ListNode(0, head);
+    ListNode* lead = sentinel;
+    ListNode* start = lead->next; // need to start->next = tail
+    ListNode* end = nullptr;      // need to lead->next = end
+
+    for (int i = 0; i < n / k; ++i) { // each iteration reverses k nodes from start to end
+        ListNode* prev = lead;
+        ListNode* iter = start;
+
+        for (int j = 1; j <= k; ++j) {
+            if (j == k) { // need to note it down to set lead->next = end
+                end = iter;
+            }
+            ListNode* next = iter->next;
+            iter->next = prev;
+            prev = iter;
+            iter = next;
+        } // now iter points to end past 1, i.e., tail
+
+        lead->next = end;
+        start->next = iter;
+
+        // set lead, start, end for next iteration
+        lead = start;
+        start = iter;
+    }
+
+    return sentinel->next;
+}
