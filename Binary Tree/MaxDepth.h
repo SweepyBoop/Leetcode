208e3d2b84ef28d4e9e833ec4641c9b61a68e5e2
SweepyBoop <78008331+SweepyBoop@users.noreply.github.com>
2024-03-24 15:26:41 -0700

    update

---
+++ Binary Tree/MaxDepth.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "..\Common.h"
+
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/description/?envType=study-plan-v2&envId=top-interview-150
+int maxDepth(TreeNode* root) {
+}
