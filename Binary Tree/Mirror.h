2203346bbdc7693fcf764b18f820fb54063de2d7
SweepyBoop <78008331+SweepyBoop@users.noreply.github.com>
2024-03-24 23:19:28 -0700

    Update Mirror.h

--- Binary Tree/Mirror.h
+++ Binary Tree/Mirror.h
@@ -31,3 +31,61 @@ bool isSymmetric(TreeNode* root) {
 
     return isMirror(root->left, root->right);
 }
+
+bool isSymmetricIterative(TreeNode* root) {
+    if (!root) {
+        return true;
+    }
+
+    if (!root->left && !root->right) {
+        return true;
+    }
+
+    if (!root->left || !root->right) {
+        return false;
+    }
+
+    // this is similar to a level order traversal, but each time we pop 2 nodes n1 and n2, and check if they are the same
+    // when pushing their children, push like this:
+    // n1.left, n2.right, n1.right, n2.left
+
+    // level order traversal uses a queue
+    queue<TreeNode*> q;
+    q.push(root->left);
+    q.push(root->right);
+
+    while (!q.empty()) {
+        int levelSize = q.size();
+        while (levelSize) {
+            TreeNode* left = q.front();
+            q.pop();
+            TreeNode* right = q.front();
+            q.pop();
+            levelSize -= 2;
+
+            if (left->val != right->val) {
+                return false;
+            }
+
+            if ((left->left == nullptr) ^ (right->right == nullptr)) {
+                return false;
+            }
+
+            if (left->left) {
+                q.push(left->left);
+                q.push(right->right);
+            }
+
+            if ((left->right == nullptr) ^ (right->left == nullptr)) {
+                return false;
+            }
+
+            if (left->right) {
+                q.push(left->right);
+                q.push(right->left);
+            }
+        }
+    }
+
+    return true;
+}
